c3efa2d1b05a839d06081d893618e6a4b0991149
Zaid Ahmed <zaidahmed6500@gmail.com>
2025-09-02 06:48:20 +0500

    changes

--- app.js
+++ app.js
@@ -1,59 +1,61 @@
- window.onload = function () {
-      let minutes = 0;
-      let seconds = 0;
-      let tens = 0o0;
-      let appendMinutes = document.querySelector('#minutes');
-      let appendTens = document.querySelector('#tens');
-      let appendSeconds = document.querySelector('#seconds');
-      let startBtn = document.querySelector('#start');
-      let stopBtn = document.querySelector('#stop');
-      let resetBtn = document.querySelector('#reset');
-      let Interval;
+window.onload = function () {
+let hour = 0;
+let minute = 0;
+let second = 0o0;
+let appendHour = document.querySelector('#hour');
+let appendMinute = document.querySelector('#minute');
+let appendSecond = document.querySelector('#second')
+let startBtn = document.querySelector('#start');
+let stopBtn = document.querySelector('#stop');
+let resetBtn = document.querySelector('#reset');
+let Interval;
 
-      const startTimer = () => {
-        tens++;
-        if (tens <= 9) {
-          appendTens.innerHTML = '0' + tens;
-        }
-        if (tens > 9) {
-          appendTens.innerHTML = tens;
-        }
+const startTimer = () => {
+    second++;
+    if (second <= 9) {
+      appendSecond.innerHTML = '0' + second;
+    }
+    if (second > 9) {
+      appendSecond.innerHTML = second;
+    }
 
-        if (tens > 99) {
-          seconds++;
-          appendSeconds.innerHTML = '0' + seconds;
-          tens = 0;
-          appendTens.innerHTML = '0' + 0;
-        }
+    if (second > 59) {
+      minute++;
+      appendMinute.innerHTML = '0' + minute;
+      second = 0;
+      appendSecond.innerHTML = '0' + 0;
+    }
 
-        if (seconds > 9) {
-          appendSeconds.innerHTML = seconds;
-        }
+    if(minute > 9){
+        appendMinute.innerHTML = minute;
+    }
 
-        if (seconds > 59) {
-          minutes++;
-          appendMinutes.innerHTML = '0' + minutes;
-          seconds = 0;
-          appendSeconds.innerHTML = '0' + 0;
-        }
-      };
+    if(minute > 59){
+        hour++;
+        appendHour.innerHTML = "0" + hour;
+        minute = 0;
+        appendMinute.innerHTML = "0" + 0;
+    }
+};
 
-      startBtn.onclick = () => {
-        clearInterval(Interval);
-        Interval = setInterval(startTimer, 10);
-      };
 
-      stopBtn.onclick = () => {
-        clearInterval(Interval);
-      };
+startBtn.onclick = () =>{
+    clearInterval(Interval);
+    Interval = setInterval(startTimer, 1000);
+};
 
-      resetBtn.onclick = () => {
-        clearInterval(Interval);
-        tens = '00';
-        seconds = '00';
-        minutes = '00';
-        appendTens.innerHTML = tens;
-        appendSeconds.innerHTML = seconds;
-        appendMinutes.innerHMTL = minutes;
-      };
-    };
+stopBtn.onclick = () =>{
+    clearInterval(Interval);
+};
+
+resetBtn.onclick = () =>{
+    clearInterval(Interval);
+    second = '00';
+    minute = '00';
+    hour = '00';
+    appendSecond.innerHTML = second;
+    appendMinute.innerHTML = minute;
+    appendHour.innerHTML = hour;
+};
+};
+
